5892f625118be4f139cf71b1511377b9be631571
agungdwiyantoro <81406588+agungdwiyantoro@users.noreply.github.com>
2022-08-25 07:25:01 +0700

    edit profile minor fix #3

--- app/src/main/java/com/devfutech/paradisonesia/presentation/fragments/edit_profile/EditProfile.kt
+++ app/src/main/java/com/devfutech/paradisonesia/presentation/fragments/edit_profile/EditProfile.kt
@@ -59,7 +59,7 @@ class EditProfile : BaseFragment(){
             })
 
             btConfirm.setOnClickListener({
-                etCalendarPick.setText(dpBirthdate.getDate().toString())
+                tieCalendarPickValue.setText(dpBirthdate.getDate().toString())
                 llcompDatePicker.visibility = View.GONE
             })
 
@@ -67,53 +67,8 @@ class EditProfile : BaseFragment(){
                 findNavController().safeNavigate(R.id.action_editProfile_to_accountFragment)
             })
 
-            btSave.setOnClickListener({
-                val validation = arrayOfNulls<Boolean>(3)
-                validation[0] = if (ilEmail.inputError(
-                        etFullNameValue.text.toString().trim(), resources.getString(
-                            R.string.empty_fields,
-                            resources.getString(R.string.label_email)
-                        )
-                    )
-                ) {
-                    if (!etEmail.text.toString().trim().isEmailValid()) {
-                        ilEmail.error = resources.getString(
-                            R.string.wrong_format,
-                            resources.getString(R.string.label_email)
-                        )
-                        false
-                    } else {
-                        true
-                    }
-                } else {
-                    false
-                }
-                validation[1] = if (ilPassword.inputError(
-                        etPassword.text.toString().trim(), resources.getString(
-                            R.string.empty_fields,
-                            resources.getString(R.string.label_password)
-                        )
-                    )
-                ) {
-                    if (etPassword.text.toString().trim().length < 8) {
-                        ilPassword.error = resources.getString(
-                            R.string.minimum_characters,
-                            resources.getString(R.string.label_password)
-                        )
-                        false
-                    } else {
-                        true
-                    }
-                } else {
-                    false
-                }
+            btSave.setOnClickListener ({
 
-                if (!validation.contains(false)) {
-                    viewModel.createFirebaseUser(
-                        email = etEmail.text.toString().trim(),
-                        password = etPassword.text.toString().trim()
-                    )
-                }
             })
 
             Firebase.auth.currentUser?.let { account ->
@@ -127,4 +82,56 @@ class EditProfile : BaseFragment(){
     }
 
 
+    fun loginCheck(){
+        binding.apply {
+            val validation = arrayOfNulls<Boolean>(6)
+            validation[0] = if (tilFullName.inputError(
+                    tieFullNameValue.text.toString().trim(),
+                    resources.getString(R.string.empty_fields,
+                        resources.getString(R.string.label_email)
+                    )
+                )
+            ) {
+                if (!tieEmailValue.text.toString().trim().isEmailValid()) {
+                    tilEmail.error = resources.getString(
+                        R.string.wrong_format,
+                        resources.getString(R.string.label_email)
+                    )
+                    false
+                } else {
+                    true
+                }
+            } else {
+                false
+            }
+
+            validation[1] = if (tilFullName.inputError(
+                    tieFullNameValue.text.toString().trim(),
+                    resources.getString(R.string.empty_fields,
+                        resources.getString(R.string.label_email)
+                    )
+                )
+            ) {
+                if (!tieEmailValue.text.toString().trim().isEmailValid()) {
+                    tilEmail.error = resources.getString(
+                        R.string.wrong_format,
+                        resources.getString(R.string.label_email)
+                    )
+                    false
+                } else {
+                    true
+                }
+            } else {
+                false
+            }
+
+
+            if (!validation.contains(false)) {
+                viewModel.createFirebaseUser(
+                    email = etEmail.text.toString().trim(),
+                    password = etPassword.text.toString().trim()
+                )
+            }
+        }
+    }
 }
